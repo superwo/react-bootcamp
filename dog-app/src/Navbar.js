09c35201e8046756727d7873f4a39bcff4e79643
Radu <avadanii@tiscali.it>
2019-06-23 14:03:33 +0300

    dog details component

---
+++ dog-app/src/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+        <a href='#' className='navbar-brand'>
+          Dog App
+        </a>
+      </nav>
+    );
+  }
+}
